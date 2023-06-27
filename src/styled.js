234dbe34c134e3944ddfdf4f2dcf80af15f28847
feedmelab <xtorner@gmail.com>
2023-06-27 23:57:54 +0200

    Final with Goggle Font

--- src/styled.js
+++ src/styled.js
@@ -1,9 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-const font3 =
-  "https://fonts.googleapis.com/css2?family=Syncopate:wght@400;700&display=swap";
-const font1 = "https://fonts.googleapis.com/css2?family=Work+Sans&display=swap";
-const font2 = "https://fonts.googleapis.com/css2?family=Pacifico&display=swap";
-const font4 = "https://fonts.googleapis.com/css2?family=Dosis&display=swap";
 
 // const getHeightDoc = () => {
 //   const { scrollHeight: height } = document.body;
@@ -15,31 +10,26 @@ const font4 = "https://fonts.googleapis.com/css2?family=Dosis&display=swap";
 export const GlobalStyle = createGlobalStyle`
     
     p {
-        font-family: "Work Sans", sans-serif;
+        font-family: "Pt Sans", sans-serif;
     }
 
 	body {
-        font-family: "Work Sans", sans-serif;    
+        font-family: "Pt Sans", sans-serif;    
 	}
     a{
         text-decoration: none !important;
-        color: inherit !important;
+        color: inherit !important
     }
-    @font-face {
-        font-family: 'Dosis', sans-serif;
-        src: url(${font4});
-    }
-    @font-face {
-        font-family: 'Syncopate', sans-serif;
-        src: url(${font3});
-    }
-    @font-face {
-        font-family: "Pacifico", sans-serif;
-        src: url(${font2});
-    }
-    @font-face {
-        font-family: "Work Sans", sans-serif;
-        src: url(${font1});
-    }
+ 
+ 
+    :root {
+  --primary-color: #2470b5 !important;
+  --border-color: #e6e6e4 !important;
+  --bar-color: #bcbcba !important;
+  --radius-size: 0.1rem !important;
+  --min-spacer: 0.3rem;
+  --common-shadow: rgba(5, 5, 93, 0.15) 0px 2px 2px -1px,
+    rgba(0, 0, 0, 0.2) 0px 1px 3px -1px;
+}
     
 `;
