59e47db50ebc63841e688e079e1df3aaca986604
feedmelab <xtorner@gmail.com>
2023-07-04 04:31:47 +0200

    Temporary adaptation to a new proxy CORS due to failure

--- src/actions/fetchActions.js
+++ src/actions/fetchActions.js
@@ -5,20 +5,18 @@ import { loadAxiosProgress } from "axios-progress";
 loadAxiosProgress(axios);
 
 const callAxios = async (url) => {
-  const response = await axios.get(url);
-  if (!response.data || !response.data.contents) {
+  const response = await fetch(url);
+  if (!response.ok) {
     throw new Error("Server response was not as expected.");
   }
-  return JSON.parse(response.data.contents);
+  return response.json();
 };
 
 export const fetchPodcastDetails = createAsyncThunk(
   "podcast/fetchPodcastDetails",
   async (podcastId, { rejectWithValue }) => {
     try {
-      const url = `https://api.allorigins.win/get?url=${encodeURIComponent(
-        `https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=20`
-      )}`;
+      const url = `https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=20`;
       const parsedData = await callAxios(url);
       return parsedData.results;
     } catch (error) {
@@ -31,9 +29,7 @@ export const fetchPodcasts = createAsyncThunk(
   "podcast/fetchPodcasts",
   async (_, { rejectWithValue }) => {
     try {
-      const url = `https://api.allorigins.win/get?url=${encodeURIComponent(
-        "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
-      )}`;
+      const url = `https://cors-anywhere.herokuapp.com/https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json`;
       const parsedData = await callAxios(url);
       const podcasts = parsedData.feed.entry.map((entry) => ({
         id: entry.id.attributes["im:id"],
@@ -51,4 +47,3 @@ export const fetchPodcasts = createAsyncThunk(
     }
   }
 );
- 
