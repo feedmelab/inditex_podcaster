e5a4ef950e10d965a187c78cc82e136ae258e5d2
feedmelab <xtorner@gmail.com>
2023-07-05 22:55:52 +0200

    Fixing Wallabys ninja missconfiguration

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import App from "./App";
-import "./index.css";
 import { GlobalStyle } from "./styled";
 import "bootstrap/dist/css/bootstrap.css";
 
